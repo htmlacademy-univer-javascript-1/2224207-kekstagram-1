2a8570d401d34b11ffbd4117250a0f68070a7776
Keks <keks@htmlacademy.ru>
2022-09-18 21:19:53 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,9 @@
+function getRandomFromRange(from = 0, to = 1) {
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+function isStringLessThen(string = '', maxLenght = 140) {
+  return string.length <= maxLenght;
+}
+
+getRandomFromRange();
+isStringLessThen();
