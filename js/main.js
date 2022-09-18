bd7cd0b1e0465785516fe75a2b170c837ec9a67d
Roaoch <103529423+Roaoch@users.noreply.github.com>
2022-09-18 23:18:38 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function getRandomFromRange(from = 0, to = 1) {
-    return Math.floor(Math.random() * (to - from + 1)) + from;
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+function isStringLessThen(string = '', maxLenght = 140) {
+  return string.length <= maxLenght;
 }
 
-function isStringLessThen(string = "", maxLenght = 140) {
-    return string.length <= maxLenght;
-}
+getRandomFromRange();
+isStringLessThen();
